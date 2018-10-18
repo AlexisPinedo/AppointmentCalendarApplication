a73a0bd35d8332978cbfad2616c11f4565534ae0
Dominique Oyco <dominickoyco@gmail.com>
2018-10-18 11:21:26 -0700

    Added login GUI

--- AppointmentCalendar/src/appointmentcalendar/accountLogin.java
+++ AppointmentCalendar/src/appointmentcalendar/accountLogin.java
@@ -7,14 +7,14 @@ package appointmentcalendar;
 
 /**
  *
- * @author pined
+ * @author Pir8
  */
-public class CalendarGUI extends javax.swing.JFrame {
+public class accountLogin extends javax.swing.JFrame {
 
     /**
-     * Creates new form CalendarGUI
+     * Creates new form accountLogin
      */
-    public CalendarGUI() {
+    public accountLogin() {
         initComponents();
     }
 
@@ -33,11 +33,11 @@ public class CalendarGUI extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 576, Short.MAX_VALUE)
+            .addGap(0, 557, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 418, Short.MAX_VALUE)
+            .addGap(0, 437, Short.MAX_VALUE)
         );
 
         pack();
@@ -60,20 +60,20 @@ public class CalendarGUI extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CalendarGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(accountLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CalendarGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(accountLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CalendarGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(accountLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CalendarGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(accountLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new CalendarGUI().setVisible(true);
+                new accountLogin().setVisible(true);
             }
         });
     }
