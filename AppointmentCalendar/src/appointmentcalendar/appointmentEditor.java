82dd3c8bfa1d227839443dfb4cd68611024cbe73
Dominique Oyco <dominickoyco@gmail.com>
2018-12-10 14:48:53 -0800

    appointmentEditor update

--- AppointmentCalendar/src/appointmentcalendar/appointmentEditor.java
+++ AppointmentCalendar/src/appointmentcalendar/appointmentEditor.java
@@ -33,44 +33,7 @@ public class appointmentEditor extends javax.swing.JFrame {
     public appointmentEditor()
     {
         initComponents();
-        setSize(720,480);
-    }
-    
-//    pID                    INT NOT NULL,
-//    pFname                 VARCHAR(20) NOT NULL,
-//    PLname                 VARCHAR(20) NOT NULL,
-//    hID                    INT NOT NULL,
-//    hospitalName           VARCHAR(50) NOT NULL,
-//    hospitalAddress        VARCHAR(50) NOT NULL,
-//    dID                    INT NOT NULL,
-//    doctorName             VARCHAR(50) NOT NULL,
-//    appointmentDateTime    VARCHAR(20),
-    
-    public void appointmentCBFillData()
-    {
-        try{
-            Class.forName(JDBC_DRIVER);
-            conn = DriverManager.getConnection(DB_URL,USER,PASS);
-            Statement statement = conn.createStatement();    
-            String sql = "Select * FROM Appointments";
-            ResultSet rs = statement.executeQuery(sql);
-            
-            while(rs.next())
-            {
-                appointmentList.addItem(Integer.toString(rs.getInt("pID")) + " " +
-                   rs.getString("pFname") + " " + rs.getString("pLname") + " " + 
-                   Integer.toString(rs.getInt("hID")) + " " 
-                    + rs.getString("hospitalName") + " " + 
-                        rs.getString("hospitalAddress") + " " +
-                        Integer.toString(rs.getInt("dID")) + " " +
-                        rs.getString("doctorName") + " " +
-                        rs.getString("appointmentDateTime"));               
-            }                          
-        }
-        catch(Exception e)
-        {
-            System.out.println(e);
-        }
+        setSize(720, 480);
     }
 
     /**
@@ -81,39 +44,35 @@ public class appointmentEditor extends javax.swing.JFrame {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
-        bindingGroup = new org.jdesktop.beansbinding.BindingGroup();
 
         entityManager = java.beans.Beans.isDesignTime() ? null : javax.persistence.Persistence.createEntityManagerFactory("343Project1testdbPU").createEntityManager();
         appointmentsQuery = java.beans.Beans.isDesignTime() ? null : entityManager.createQuery("SELECT a FROM Appointments a");
         appointmentsList = java.beans.Beans.isDesignTime() ? java.util.Collections.emptyList() : appointmentsQuery.getResultList();
         appointmentsQuery1 = java.beans.Beans.isDesignTime() ? null : entityManager.createQuery("SELECT a FROM Appointments a");
         appointmentsList1 = java.beans.Beans.isDesignTime() ? java.util.Collections.emptyList() : appointmentsQuery1.getResultList();
+        appointmentsQuery2 = java.beans.Beans.isDesignTime() ? null : entityManager.createQuery("SELECT a FROM Appointments a");
+        appointmentsList2 = java.beans.Beans.isDesignTime() ? java.util.Collections.emptyList() : appointmentsQuery2.getResultList();
         jLabel1 = new javax.swing.JLabel();
-        appointmentList = new javax.swing.JComboBox<>();
         jLabel2 = new javax.swing.JLabel();
-        userEmail = new javax.swing.JTextField();
+        emailText = new javax.swing.JTextField();
         validateButton = new javax.swing.JButton();
         changeLocationButton = new javax.swing.JButton();
         changeDoctorButton = new javax.swing.JButton();
         changeTimeButton = new javax.swing.JButton();
+        appointmentList = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLabel1.setText("Choose an appointment to change");
 
-        org.jdesktop.beansbinding.ELProperty eLProperty = org.jdesktop.beansbinding.ELProperty.create("${}");
-        org.jdesktop.swingbinding.JComboBoxBinding jComboBoxBinding = org.jdesktop.swingbinding.SwingBindings.createJComboBoxBinding(org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ_WRITE, appointmentsList1, eLProperty, appointmentList);
-        bindingGroup.addBinding(jComboBoxBinding);
-
-        appointmentList.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                appointmentListActionPerformed(evt);
-            }
-        });
-
         jLabel2.setText("Enter your email address");
 
         validateButton.setText("VALIDATE");
+        validateButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                validateButtonActionPerformed(evt);
+            }
+        });
 
         changeLocationButton.setText("CHANGE LOCATION");
         changeLocationButton.addActionListener(new java.awt.event.ActionListener() {
@@ -141,50 +100,48 @@ public class appointmentEditor extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addGap(34, 34, 34)
+                .addComponent(appointmentList, javax.swing.GroupLayout.PREFERRED_SIZE, 615, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(51, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel2)
+                    .addComponent(emailText, javax.swing.GroupLayout.PREFERRED_SIZE, 301, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 235, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(99, 99, 99)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel2)
-                            .addComponent(userEmail, javax.swing.GroupLayout.PREFERRED_SIZE, 301, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 235, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(101, 101, 101)
-                                .addComponent(validateButton))))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(34, 34, 34)
-                        .addComponent(appointmentList, javax.swing.GroupLayout.PREFERRED_SIZE, 474, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(54, 54, 54)
-                        .addComponent(changeLocationButton)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(changeTimeButton)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(changeDoctorButton)))
-                .addContainerGap(27, Short.MAX_VALUE))
+                        .addGap(101, 101, 101)
+                        .addComponent(validateButton)))
+                .addGap(197, 197, 197))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(changeLocationButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(changeTimeButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(changeDoctorButton)
+                .addGap(131, 131, 131))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(41, 41, 41)
+                .addGap(35, 35, 35)
                 .addComponent(jLabel2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(userEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(emailText, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(validateButton)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(appointmentList, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 110, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 141, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(changeLocationButton)
                     .addComponent(changeTimeButton)
                     .addComponent(changeDoctorButton))
                 .addGap(70, 70, 70))
         );
-
-        bindingGroup.bind();
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -194,10 +151,6 @@ public class appointmentEditor extends javax.swing.JFrame {
         WindowEvent winClosing = new WindowEvent(this, WindowEvent.WINDOW_CLOSING);
     }
     
-    private void appointmentListActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_appointmentListActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_appointmentListActionPerformed
-
     
     private void changeTimeButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_changeTimeButtonActionPerformed
         // TODO add your handling code here:
@@ -210,6 +163,50 @@ public class appointmentEditor extends javax.swing.JFrame {
     private void changeLocationButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_changeLocationButtonActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_changeLocationButtonActionPerformed
+
+    
+    private void validation()
+    {
+        try{
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+//            Statement statement = conn.createStatement();    
+            String sql = "Select hospitalName, hospitalAddress, doctorName,"
+                    + "appointmentDateTime FROM Appointments "
+                    + "NATURAL JOIN Patients WHERE pEmail = ?";
+            PreparedStatement statement = conn.prepareStatement(sql);
+//            ResultSet rs = statement.executeQuery(sql);
+            
+            statement.clearParameters();
+            statement.setString(1,emailText.getText());
+            ResultSet rs = statement.executeQuery();
+            
+            while(rs.next())
+            {
+                JOptionPane.showMessageDialog(null, "Email address is valid!");
+//                appointmentList.addItem(Integer.toString(rs.getInt("pID")) + " " +
+//                   rs.getString("pFname") + " " + rs.getString("pLname") + " " + 
+//                   Integer.toString(rs.getInt("hID")) + " " 
+//                    + rs.getString("hospitalName") + " " + 
+//                        rs.getString("hospitalAddress") + " " +
+//                        Integer.toString(rs.getInt("dID")) + " " +
+//                        rs.getString("doctorName") + " " +
+//                        rs.getString("appointmentDateTime"));     
+                appointmentList.addItem(rs.getString("hospitalName") + " " + 
+                        rs.getString("hospitalAddress") + " " +
+                        rs.getString("doctorName") + " " +
+                        rs.getString("appointmentDateTime"));  
+            }                          
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }   
+    }
+    private void validateButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_validateButtonActionPerformed
+
+            validation();           
+    }//GEN-LAST:event_validateButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -251,16 +248,17 @@ public class appointmentEditor extends javax.swing.JFrame {
     private javax.swing.JComboBox<String> appointmentList;
     private java.util.List<appointmentcalendar.Appointments> appointmentsList;
     private java.util.List<appointmentcalendar.Appointments> appointmentsList1;
+    private java.util.List<appointmentcalendar.Appointments> appointmentsList2;
     private javax.persistence.Query appointmentsQuery;
     private javax.persistence.Query appointmentsQuery1;
+    private javax.persistence.Query appointmentsQuery2;
     private javax.swing.JButton changeDoctorButton;
     private javax.swing.JButton changeLocationButton;
     private javax.swing.JButton changeTimeButton;
+    private javax.swing.JTextField emailText;
     private javax.persistence.EntityManager entityManager;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JTextField userEmail;
     private javax.swing.JButton validateButton;
-    private org.jdesktop.beansbinding.BindingGroup bindingGroup;
     // End of variables declaration//GEN-END:variables
 }
